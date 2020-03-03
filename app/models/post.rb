0adfb2cb23e62ce1a8370dda6162d068fae3a2bb
Daniel Matney <danieljmatney@gmail.com>
2020-03-03 19:24:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, inclusion: { minimum: 100 }
-  validates :category, inclusion: { "Fiction" || "Non-Fiction" }
+  validates :category, inclusion: %w{Fiction Non-Fiction}
 end
